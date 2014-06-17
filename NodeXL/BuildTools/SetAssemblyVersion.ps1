3941edc645531eb4357c34169c69714506ac31c2
tcap479_cp <SND\tcap479_cp>
2014-06-17 22:19:59 +0000

    Increment version number to 1.0.1.329.

--- NodeXL/BuildTools/SetAssemblyVersion.ps1
+++ NodeXL/BuildTools/SetAssemblyVersion.ps1
@@ -116,4 +116,4 @@ function Main()
 	ProcessExcelTemplateCsprojFile $sPathRoot $iRevision
 }
 
-Main(328)
+Main(329)
--- NodeXL/GraphDataProviders/Properties/AssemblyInfo.cs
+++ NodeXL/GraphDataProviders/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.1.328")]
-[assembly: AssemblyFileVersion("1.0.1.328")]
+[assembly: AssemblyVersion("1.0.1.329")]
+[assembly: AssemblyFileVersion("1.0.1.329")]
