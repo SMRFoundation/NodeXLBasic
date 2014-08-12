60650cdd80227e3ea0693c8249c1f4d9712145b4
tcap479_cp <SND\tcap479_cp>
2014-08-12 04:03:56 +0000

    Increment version number to 1.0.1.333.

--- NodeXL/GraphDataProviders/Properties/AssemblyInfo.cs
+++ NodeXL/GraphDataProviders/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.1.332")]
-[assembly: AssemblyFileVersion("1.0.1.332")]
+[assembly: AssemblyVersion("1.0.1.333")]
+[assembly: AssemblyFileVersion("1.0.1.333")]
